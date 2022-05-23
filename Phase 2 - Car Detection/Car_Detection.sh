0e8b058c11e038a52f8f021876976b8462bcc662
HASSAN-MOHSEN <105822617+HASSAN-MOHSEN@users.noreply.github.com>
2022-05-23 22:40:48 +0200

    Add files via upload

---
+++ Phase 2 - Car Detection/Car_Detection.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#First Argument 
+#Input Video Path
+input="C:\Users\Bishoy Anwar\OneDrive\Desktop\project_video.mp4"
+
+#Second Argument
+#Output Video Path
+output="Project Video"
+
+ipython VehicleTracking.py "$input" "$output"
